416e36c863f0126aa7cf3d69522eaab253eb95fa
Natalie-Winans <52257335+Natalie-Winans@users.noreply.github.com>
2021-02-07 10:43:35 -0800

    Update sort.sh

--- sort.sh
+++ sort.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+#sort input sam file by UMI, GNAME, POS
 grep '@' $1 > sorted_temp.sam
 grep -v '@' $1 | awk -F'[:\t]' '{print $8"\t"$10"\t"$11"\t"$0}' | sort -k1,1 -k2,2n -k3,3n | cut -f 4- >> sorted_temp.sam
